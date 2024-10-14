c5f7cd18c6366b0016f175c20e2702bb04c481aa
Saiganesh Sristla <111565089+saiganesh-sristla@users.noreply.github.com>
2024-10-14 11:10:42 +0530

    Update 9_RFID.py

--- practical/9_RFID.py
+++ practical/9_RFID.py
@@ -1,3 +1,44 @@
+'''
+sudo raspi-config
+
+sudo apt-get update
+sudo apt-get install libusb-dev libpcsclite-dev i2c-tools
+
+cd ~
+wget https://github.com/nfc-tools/libnfc/releases/download/libnfc-1.7.2/libnfc-1.7.2.tar.bz2
+tar -xf libnfc-1.7.2.tar.bz2  
+
+cd libnfc-1.7.2 
+./configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install 
+
+cd /etc
+sudo mkdir nfc
+sudo vim /etc/nfc/libnfc.conf
+
+Add the following in the conf file
+#allow_autoscan = true
+#allow_intrusive_scan = false
+#log_level = 1
+#device.name = "_PN532_I2c" 
+#device.connstring = "pn532_i2c:/dev/i2c-1"
+
+5V    4 
+GND   6 
+SDA   3 
+SCL   5 
+
+i2cdetect –yes 1 
+nfc-list
+nfc-poll
+
+'''
+
+
+
+
+
 import subprocess
 import time
 
@@ -22,4 +63,5 @@ try:
         id_str=str[2]+str[3]+str[4]+str[5]
         print (id_str)
 except KeyboardInterrupt:
-    pass
+    pass
+
